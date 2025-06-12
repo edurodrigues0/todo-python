936cde193a38808e28264e0e6d9f675fadce20e9
edurodrigues0 <edusacramento9@gmail.com>
2025-06-11 21:51:44 -0300

    Add task manager menu implementation

---
+++ gerenciador.py
@@ -0,0 +1,15 @@
+while True:
+  print("\n Menu de Gerenciador de Tarefas:")
+  print("1. Adicionar tarefa")
+  print("2. Ver tarefas")
+  print("3. Atualizar tarefa")
+  print("4. Completar tarefa")
+  print("5. Deletar tarefas completadas")
+  print("6. Sair")
+
+  escolha = input("Escolha uma opção: ")
+  
+  if escolha == "6":
+    break
+  
+print("Programa finalizado.")
