1379033e2a6058170a857258c3b75e0e7bca770e
edurodrigues0 <edusacramento9@gmail.com>
2025-06-16 19:46:32 -0300

    Refactor task management functions to improve clarity and functionality

--- gerenciador.py
+++ gerenciador.py
@@ -27,11 +27,37 @@ def show_tasks(tasks):
   clear_terminal()
   print("Lista de tarefas:")
   for task in tasks:
-  print(f"ID: {task['id']}")
-  print(f"Nome: {task['name']}")
-  print(f"Descrição: {task['description']}")
-  print(f"Status: {'Concluída' if task['is_completed'] else 'Pendente'}")
-  print("-" * 20)
+    print(f"ID: {task['id']}")
+    print(f"Nome: {task['name']}")
+    print(f"Descrição: {task['description']}")
+    print(f"Status: {'Concluída' if task['is_completed'] else 'Pendente'}")
+    print("-" * 20)
+  return
+
+def update_task(tasks, task_id, new_name, new_description):
+  for task in tasks:
+    if task["id"] == task_id:
+      task["name"] = new_name
+      task["description"] = new_description
+      clear_terminal()
+      print("Tarefa atualizada com sucesso!")
+      return
+
+def complete_task(tasks, task_id):
+  for task in tasks:
+    if task["id"] == task_id:
+      task["is_completed"] = True
+      name = task["name"]
+      clear_terminal()
+      print(f"Tarefa {name} marcada como concluida")
+
+def delete_tasks(tasks):
+  for task in tasks:
+    if task["is_completed"]:
+      tasks.remove(task)
+
+  clear_terminal()
+  print("Tarefas completadas deletadas com sucesso!")
 
 tasks = []
 
@@ -51,6 +77,16 @@ while True:
     add_tasks(tasks, task_name, task_description)
   elif choice == "2":
     show_tasks(tasks)
+  elif choice == "3":
+    task_id = int(input("Digite o ID da tarefa que deseja atualizar: "))
+    new_name = input("Digite o novo nome da tarefa: ")
+    new_description = input("Digite a nova descrição da tarefa: ")
+    update_task(tasks, task_id, new_name, new_description)
+  elif choice == "4":
+    task_id = int(input("Digite o ID da tarefa que deseja completar: "))
+    complete_task(tasks, task_id)
+  elif choice == "5":
+    delete_tasks(tasks)
   elif choice == "6":
     break
   
